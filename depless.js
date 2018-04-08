bc5a82aaa7e697c46eab500d7b81447fba03dbe7
Thomas Deniffel <tdeniffel@googlemail.com>
2018-04-08 22:51:32 +0200

    Routing implemented

--- depless.js
+++ depless.js
@@ -186,3 +186,10 @@ function runScenarios() {
         }
     }
 }
+
+
+// on('application-started', function(args) {
+//     var pages = document.getElementsByClassName("page");
+//     for(var i=0; i<pages.length; i++)
+//         pages[i].style.display = "none";
+// });
--- routing.js
+++ routing.js
@@ -1,7 +1,31 @@
 (function routing() {
 
     on('routing', function(args) {
-        console.log('Route to ', args[0]);
-    })
+        var pages = document.getElementsByClassName("page");
+        for(var i=0; i<pages.length; i++) {
+            if(pages[i].id === args[0])
+                pages[i].style.display = "block";
+            else
+                pages[i].style.display = "none";
+        }
+    });
+
+    function routeFromUrl() {
+        var hash = window.location.hash;
+        if(!hash)
+            return null;
+        return hash.substr(1);
+    }
+
+    on('application-started', function(args) {
+        var route = routeFromUrl();
+
+        console.log(route);
+
+        if(route)
+            return ['routing', route];
+        else
+            return['default-route']
+    });
 
 })();
